87244d99f9a105af78a9a5bfe7458cf5a9f8038a
YOSHIMURA Yuu <yyu@mental.poker>
2022-01-16 00:02:41 +0900

    Update scalafmt-core to 3.3.1 (#21)
    * Update scalafmt-core to 3.3.1
    
    * Reformat with scalafmt 3.3.1

--- project/UpdateReadme.scala
+++ project/UpdateReadme.scala
@@ -1,4 +1,6 @@
-import sbt._, Keys._
+import sbt._
+import Keys._
+
 import sbtrelease.ReleasePlugin.autoImport.ReleaseStep
 import sbtrelease.Git
 
@@ -8,26 +10,30 @@ object UpdateReadme {
   val updateReadmeTask = { state: State =>
     val extracted = Project.extract(state)
     val v = extracted get version
-    val org =  extracted get organization
+    val org = extracted get organization
     val n = extracted get name
     val baseDir = extracted get baseDirectory
     val readmeFile = baseDir / "README.md"
 
     val str = s"""$org" % "$n" % "$v""""
-    
-    val newReadme = Predef.augmentString(IO.read(readmeFile)).lines.map{ line =>
-      if (v.contains(snapshotMark))
-        line.replaceFirst(s"""(?<=addSbtPlugin\\().+ % .+ % [^-]+-$snapshotMark"(?=\\))""", str)
-      else
-        line.replaceFirst(s"""(?<=addSbtPlugin\\().+ % .+ % [^-]+(?!-$snapshotMark)"(?=\\))""", str)
-    }.mkString("", "\n", "\n")
-    
+
+    val newReadme = Predef
+      .augmentString(IO.read(readmeFile))
+      .lines
+      .map { line =>
+        if (v.contains(snapshotMark))
+          line.replaceFirst(s"""(?<=addSbtPlugin\\().+ % .+ % [^-]+-$snapshotMark"(?=\\))""", str)
+        else
+          line.replaceFirst(s"""(?<=addSbtPlugin\\().+ % .+ % [^-]+(?!-$snapshotMark)"(?=\\))""", str)
+      }
+      .mkString("", "\n", "\n")
+
     IO.write(readmeFile, newReadme)
 
     val git = new Git(baseDir)
     git.add(readmeFile.getName) ! state.log
     git.commit(message = "Update " + readmeFile.getName, sign = false, signOff = false) ! state.log
-    
+
     state
   }
 
